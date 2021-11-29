823e7bcb1230546490cd570ff17e27c40df6b513
jerso <jersoroka@gmail.com>
2021-11-28 20:08:59 -0800

    Refactor maze input in tests to be correct type

--- src/__test__/MazeSolver.spec.ts
+++ src/__test__/MazeSolver.spec.ts
@@ -1,13 +1,52 @@
 import { isValidPos, solveMaze, solveMazeHelper } from '../components/MazeSolver';
+import { Square } from '../types';
 
 describe('Maze solver helper functions', () => {
-    const maze5x5: number[][] = [
-        [1, 1, 1, 1, 0],
-        [1, 0, 1, 0, 1],
-        [0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 0],
-        [0, 1, 1, 1, 1]
-    ];
+    // const maze5x5: number[][] = [
+    //     [1, 1, 1, 1, 0],
+    //     [1, 0, 1, 0, 1],
+    //     [0, 0, 1, 1, 0],
+    //     [0, 0, 0, 1, 0],
+    //     [0, 1, 1, 1, 1]
+    // ];
+
+    const maze5x5: Square[][] = [
+        [
+            {id: 1, pos: {row: 0, column: 0}, isMazeWall: false},
+            {id: 2, pos: {row: 0, column: 1}, isMazeWall: false},
+            {id: 3, pos: {row: 0, column: 2}, isMazeWall: false},
+            {id: 4, pos: {row: 0, column: 3}, isMazeWall: false},
+            {id: 5, pos: {row: 0, column: 4}, isMazeWall: true}
+        ],
+        [
+            {id: 6, pos: {row: 1, column: 0}, isMazeWall: false},
+            {id: 7, pos: {row: 1, column: 1}, isMazeWall: true},
+            {id: 8, pos: {row: 1, column: 2}, isMazeWall: false},
+            {id: 9, pos: {row: 1, column: 3}, isMazeWall: true},
+            {id: 10, pos: {row: 1, column: 4}, isMazeWall: false}
+        ],
+        [
+            {id: 11, pos: {row: 2, column: 0}, isMazeWall: true},
+            {id: 12, pos: {row: 2, column: 1}, isMazeWall: true},
+            {id: 13, pos: {row: 2, column: 2}, isMazeWall: false},
+            {id: 14, pos: {row: 2, column: 3}, isMazeWall: false},
+            {id: 15, pos: {row: 2, column: 4}, isMazeWall: true}
+        ],
+        [
+            {id: 16, pos: {row: 3, column: 0}, isMazeWall: true},
+            {id: 17, pos: {row: 3, column: 1}, isMazeWall: true},
+            {id: 18, pos: {row: 3, column: 2}, isMazeWall: true},
+            {id: 19, pos: {row: 3, column: 3}, isMazeWall: false},
+            {id: 20, pos: {row: 3, column: 4}, isMazeWall: true}
+        ],
+        [
+            {id: 21, pos: {row: 4, column: 0}, isMazeWall: true},
+            {id: 22, pos: {row: 4, column: 1}, isMazeWall: false},
+            {id: 23, pos: {row: 4, column: 2}, isMazeWall: false},
+            {id: 24, pos: {row: 4, column: 3}, isMazeWall: false},
+            {id: 25, pos: {row: 4, column: 4}, isMazeWall: false}
+        ],
+    ]
 
     const initialSol5x5: number[][] = [
         [0, 0, 0, 0, 0],
@@ -16,14 +55,45 @@ describe('Maze solver helper functions', () => {
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0]
     ];
-
-    const maze5x5WithNoSol: number[][] = [
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0]
-    ];
+    // FIXME: 1 is path
+    
+    const maze5x5WithNoSol: Square[][] = [
+        [
+            {id: 1, pos: {row: 0, column: 0}, isMazeWall: true},
+            {id: 2, pos: {row: 0, column: 1}, isMazeWall: false},
+            {id: 3, pos: {row: 0, column: 2}, isMazeWall: false},
+            {id: 4, pos: {row: 0, column: 3}, isMazeWall: false},
+            {id: 5, pos: {row: 0, column: 4}, isMazeWall: false}
+        ],
+        [
+            {id: 6, pos: {row: 1, column: 0}, isMazeWall: true},
+            {id: 7, pos: {row: 1, column: 1}, isMazeWall: false},
+            {id: 8, pos: {row: 1, column: 2}, isMazeWall: false},
+            {id: 9, pos: {row: 1, column: 3}, isMazeWall: false},
+            {id: 10, pos: {row: 1, column: 4}, isMazeWall: false}
+        ],
+        [
+            {id: 11, pos: {row: 2, column: 0}, isMazeWall: true},
+            {id: 12, pos: {row: 2, column: 1}, isMazeWall: false},
+            {id: 13, pos: {row: 2, column: 2}, isMazeWall: false},
+            {id: 14, pos: {row: 2, column: 3}, isMazeWall: false},
+            {id: 15, pos: {row: 2, column: 4}, isMazeWall: false}
+        ],
+        [
+            {id: 16, pos: {row: 3, column: 0}, isMazeWall: true},
+            {id: 17, pos: {row: 3, column: 1}, isMazeWall: false},
+            {id: 18, pos: {row: 3, column: 2}, isMazeWall: false},
+            {id: 19, pos: {row: 3, column: 3}, isMazeWall: false},
+            {id: 20, pos: {row: 3, column: 4}, isMazeWall: false}
+        ],
+        [
+            {id: 21, pos: {row: 4, column: 0}, isMazeWall: true},
+            {id: 22, pos: {row: 4, column: 1}, isMazeWall: false},
+            {id: 23, pos: {row: 4, column: 2}, isMazeWall: false},
+            {id: 24, pos: {row: 4, column: 3}, isMazeWall: false},
+            {id: 25, pos: {row: 4, column: 4}, isMazeWall: false}
+        ],
+    ]
 
     const solvedMaze5x5: number[][] = [
         [1, 1, 1, 0, 0],
@@ -36,7 +106,7 @@ describe('Maze solver helper functions', () => {
     beforeAll(() => {
     });
 
-    describe('isValidPos', () => {
+    describe('isValidPos', () => { // fixed
         it("should be true when (x, y) is at start or cheese", () => {
             expect(isValidPos(maze5x5, 0, 0)).toBeTruthy();
             expect(isValidPos(maze5x5, maze5x5.length - 1, maze5x5.length - 1)).toBeTruthy();
@@ -54,7 +124,7 @@ describe('Maze solver helper functions', () => {
         });
     });
 
-    describe('solveMazeHelper', () => {
+    describe('solveMazeHelper', () => { //TODO: will need to be refactored when you replace return type
         let passedSol5x5: number[][];
 
         beforeEach(() => {
