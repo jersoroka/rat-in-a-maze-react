87a4d4a5088ef447a2e2d2330efd87e2264a882d
jerso <jersoroka@gmail.com>
2021-12-11 15:38:49 -0800

    Add clear maze and set maze buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Grid from './components/Grid';
 import './App.css';
 import { GlobalContext } from './context/GlobalState';
 
 function App() {
-  const { solveMaze } = useContext(GlobalContext);
+  const { restartMaze, setMazeLength, solveMaze } = useContext(GlobalContext);
+  const [length, setLength] = useState(10);
+
+  useEffect(() => {
+    setMazeLength(length);
+  }, [length])
 
   return (
       <main>
         <section className="container">
           <Grid/>
           <button onClick={() => solveMaze()}>Solve</button>
+          <button onClick={() => restartMaze()}>Restart</button>
+          <label for="mazeSize">Enter the size of the maze</label>
+          <input for="mazeSize" type="number" min="2" max="100" onChange={e => {setLength(e.target.value)}}/>
         </section>
       </main>
   );
