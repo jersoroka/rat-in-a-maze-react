d4c18a2041a5f1ed24aacbe0771b7bccf2b3e692
jerso <jersoroka@gmail.com>
2021-12-13 19:38:29 -0800

    Slow render on toy function works

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import { useContext, useEffect } from 'react';
 import Grid from './components/Grid';
 import './App.css';
 import { GlobalContext } from './context/GlobalState';
+import { cloneDeep } from 'lodash';
+import { flushSync } from 'react-dom';
 
 function App() {
-  const { restartMaze, solveMaze } = useContext(GlobalContext);
+  const { grid, restartMaze, solveMaze, setIsRoute } = useContext(GlobalContext);
+
+  useEffect(() => {
+    console.log("changed")
+  }, [grid])
+
+  async function toyColourChange() {
+    for (let i = 0; i < 5; i++) {
+      let square;
+      square = cloneDeep(grid[i][i]);
+      square.isRoute = true;
+      await new Promise(res => setTimeout(res, 1000))
+      // flushSync(() => setIsRoute(square.pos, square.isRoute));
+      setIsRoute(square.pos, square.isRoute);
+    }
+
+    // let square;
+    // square = cloneDeep(grid[0][0]);
+    // square.isRoute = true;
+    // flushSync(() => setIsRoute(square))
+
+    // square = cloneDeep(grid[1][1]);
+    // square.isRoute = true;
+    // flushSync(() => setIsRoute(square))
+    
+  }
 
   return (
       <main>
         <section className="container">
           <Grid/>
-          <button onClick={() => solveMaze()}>Solve</button>
+          {/* <button onClick={() => solveMaze()}>Solve</button> */}
           <button onClick={() => restartMaze()}>Restart</button>
+          <button onClick={(() => toyColourChange())}>Tester</button>
         </section>
       </main>
   );
