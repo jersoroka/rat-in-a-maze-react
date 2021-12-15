5b88b820fd46f438681120df709f987dae8fc8ad
jerso <jersoroka@gmail.com>
2021-12-14 20:10:40 -0800

    Set up passing setIsRoute

--- src/components/MazeSolver.ts
+++ src/components/MazeSolver.ts
@@ -5,7 +5,7 @@ export function isValidPos(maze: Square[][], x: number, y: number): boolean {
     return (x >= 0 && x < maze.length && y >= 0 && y < maze.length && !maze[x][y].isMazeWall)
 };
 
-export function solveMazeHelper(maze: Square[][], x: number, y: number, solMaze: Square[][]): boolean {
+export function solveMazeHelper(maze: Square[][], x: number, y: number, solMaze: Square[][], setIsRoute: ((pos: {column: number, row: number}, isRoute: boolean) => void)): boolean {
 
     if (x == maze.length - 1 && y == maze.length - 1) {
         solMaze[x][y].isRoute = true;
@@ -15,7 +15,7 @@ export function solveMazeHelper(maze: Square[][], x: number, y: number, solMaze:
     if (isValidPos(maze, x, y) && solMaze[x][y].isRoute != true) {
         solMaze[x][y].isRoute = true;
 
-        if (solveMazeHelper(maze, x + 1, y, solMaze) || solveMazeHelper(maze, x, y + 1, solMaze) 
+        if (solveMazeHelper(maze, x + 1, y, solMaze, setIsRoute) || solveMazeHelper(maze, x, y + 1, solMaze, setIsRoute) 
         ) {
             return true;
         }
@@ -25,7 +25,7 @@ export function solveMazeHelper(maze: Square[][], x: number, y: number, solMaze:
     return false;
 };
 
-export function solveMaze(maze: Square[][]): (Square[][] | false) {
+export function solveMaze(maze: Square[][], setIsRoute: ((pos: {column: number, row: number}, isRoute: boolean) => void)): (Square[][] | false) {
     let solMaze = cloneDeep(maze);
     solMaze.forEach(row => {
         row.forEach(square => {
@@ -33,7 +33,12 @@ export function solveMaze(maze: Square[][]): (Square[][] | false) {
         });
     })
 
-    if (!solveMazeHelper(maze, 0, 0, solMaze)) {
+    // finish passing along setIsRoute
+    // make a commit
+    // add setIsRoute to a place or two to test
+    // make a commit
+
+    if (!solveMazeHelper(maze, 0, 0, solMaze, setIsRoute)) {
         return false;
     };
     return solMaze;
