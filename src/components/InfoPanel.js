97a9a1cdfb5517eef0c45a86e0d3c95892f807c1
jerso <jersoroka@gmail.com>
2021-12-26 13:36:29 -0800

    Complete info panel

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -5,7 +5,15 @@ function InfoPanel({setIsInfoPanelShown}) {
 
     return (
         <div className="infopanel">
-            <p className="infopanel-text">What is the Rat in a Maze problem?</p>
+            <h4>What is the Rat in a Maze Problem?</h4>
+
+            <span>The Rat in a Maze problem is a popular problem that can be solved using a backtracking algorithm. The goal is to find a path in the maze from the rat to the cheese, given the following conditions:</span>
+
+            <ul>
+                <li>A rat is at the top-left cell</li>
+                <li>The destination (the cheese) is at the bottom-right cell</li>
+                <li>The rat can only move right or down</li>
+            </ul>
             <Button text="CLOSE" isMini={true} handleClick={() => setIsInfoPanelShown(false)}/>
         </div>
         
